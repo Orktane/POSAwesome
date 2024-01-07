28deb36b3e7a9077a6a91ae6f18180611de8c3b1
Orktane <110713181+Orktane@users.noreply.github.com>
2024-01-07 23:26:38 +0300

    setup.py
    Requirements changed

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in posawesome/__init__.py
 from posawesome import __version__ as version
@@ -16,5 +14,4 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires,
 )
